02b4032d9e1f940452aa88a52c04b392b2757c8e
Kacperek <remzak.k@gmail.com>
2022-03-21 12:32:41 +0100

    sex!@#!%!

--- dbsetup.py
+++ dbsetup.py
@@ -1,11 +1,29 @@
 # sqlite 3 database setup
 import sqlite3
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.sqlite')
 c = conn.cursor()
 
-c.execute("""CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nick text,
-    password text,
-    email text
-)""")
+# c.execute("""CREATE TABLE users (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nick text,
+#     password text,
+#     email text
+# )""")
+
+# create table posts id unique content img description text user_id integer upvotes int downvotes int
+# c.execute("""CREATE TABLE posts (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     title text NOT NULL,
+#     description text NOT NULL,
+#     content BLOB NOT NULL,
+#     user_id integer NOT NULL,
+#     upvotes int DEFAULT 0,
+#     downvotes int DEFAULT 0
+# )""")
+
+# add date colum to posts
+# c.execute("""ALTER TABLE posts ADD COLUMN date text""")
+
+# delete all records from posts
+c.execute("""DELETE FROM posts""")
+conn.commit()
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
+import base64
 
-conn = sqlite3.connect('database.db',check_same_thread=False)
+conn = sqlite3.connect('database.sqlite', check_same_thread=False)
 c = conn.cursor()
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 10 latest posts
+    c.execute("SELECT * FROM posts ORDER BY id DESC LIMIT 10")
+    posts = c.fetchall()
+    return render_template('index.html',posts = posts)
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -19,10 +25,12 @@ def register():
         nick = request.form['nck']
         password = request.form['pswrd']
         email = request.form['eml']
-        c.execute("INSERT INTO users (nick, password, email) VALUES (?, ?, ?)", (nick, password, email))
+        c.execute("INSERT INTO users (nick, password, email) VALUES (?, ?, ?)",
+                  (nick, password, email))
         conn.commit()
         return redirect(url_for('login'))
     return render_template('register.html')
+
 
 @app.route('/user')
 def user():
@@ -31,8 +39,8 @@ def user():
         nick = user['nick']
         email = user['email']
         password = user['password']
-        print(nick)
-        return render_template('user.html', nick=nick, email=email, password=password)
+        user_id = user['id']
+        return render_template('user.html', nick=nick, email=email, password=password, id = user_id)
     else:
         return redirect(url_for('index'))
 
@@ -48,21 +56,37 @@ def login():
         if data is None:
             return redirect(url_for('index'))
         else:
-            print(data)
             session['user'] = {}
             session['user']['nick'] = data[1]
             session['user']['password'] = password
             session['user']['email'] = email
+            session['user']['id'] = data[0]
             return redirect(url_for('user'))
     return render_template('login.html')
 
+
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
-    if request.method == "POST":
-        session.pop('user', None)
-        return redirect(url_for('index'))
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+
+@app.route("/create", methods=['POST', 'GET'])
+def create():
+    if "user" in session:
+        if request.method == 'POST':
+            title = request.form['title']
+            description = request.form['description']
+            content = base64.b64encode(request.files['file'].stream.read()).decode("utf-8")
+            user_id = session['user']['id']
+            date = datetime.now()
+            c.execute("INSERT INTO posts (title, description, content, user_id,date)VALUES (?, ?, ?, ?, ?)",
+                      (title, description, content, user_id, date))
+            conn.commit()
+            return redirect(url_for('index'))
+        return render_template('create.html')
     else:
-        return render_template('index.html')
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
